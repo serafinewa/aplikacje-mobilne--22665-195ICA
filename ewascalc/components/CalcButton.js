2a047ccf11f0bdef77b62ebd832eecdb773c0fb1
serafinewa <57776398+serafinewa@users.noreply.github.com>
2022-03-20 17:56:38 +0100

    lab1

---
+++ ewascalc/components/CalcButton.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+export default class CalcButton extends React.Component {
+
+  static defaultProps = {
+    onPress: function() { },
+    title: "",
+    color: "white",
+    backgroundColor: "black",
+    radius: 40,
+
+    style: { },
+  }
+  // ustawienie parametrów przycisków kalkulatora
+
+  render() {
+    var r = this.props.radius;
+    var w = this.props.radius * 2;
+    var h = this.props.radius * 2;
+    var bc = this.props.backgroundColor;
+
+    return (
+      <TouchableOpacity onPress={this.props.onPress}
+        style={[styles.container, { width: w, height: h, borderRadius: r, backgroundColor: bc }, { ...this.props.style }]}>
+        <Text style={[styles.text, { color: this.props.color }]}>{this.props.title}</Text>
+      </TouchableOpacity>
+    );
+    // po wciśnięciu przycisku staje się on prawie przezroczysty, korzystając z TouchableOpacity
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { alignItems: "center", justifyContent: "center", margin: 5, },
+  text: { fontSize: 20, fontWeight: "bold", },
+});
+// ustawienie czcionki cyferek i znaków kalkulatora
